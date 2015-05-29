30a830faa0604cda1c2379700fbb80a6f9980b5e
Sasidhar Movva <smovva@gmail.com>
2015-05-29 16:40:30 -0600

    fixed compile issues

--- hardware/test.ino
+++ hardware/test.ino
@@ -7,19 +7,19 @@ int setPinMode(String args)
   unsigned int  mode = 0;
 
 
-  loc = command.indexOf(",");
-  pin = command.substring(0,loc).toInt();
+  loc = args.indexOf(",");
+  pin = args.substring(0,loc).toInt();
 
-  mode = command.substring(loc+1).toInt();
+  mode = args.substring(loc+1).toInt();
   // Error check
   if(mode != 0 && mode != 1)
   {
     //invalid mode
-    return 0;
+    return 1;
   }
   if(pin > 16)
   {
-    return 0;
+    return 1;
   }
   if(mode == 0)
   {
@@ -29,6 +29,7 @@ int setPinMode(String args)
   {
     pinMode(pin,INPUT);
   }
+  return 0;
 }
 int toggleLight(String args)
   {
@@ -63,4 +64,3 @@ void loop() {
     }
     delay(1000);
 }
-
