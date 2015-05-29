762f71daea9c4d86bc0b4ade5bcbba0d851739f4
Sasidhar Movva <smovva@gmail.com>
2015-05-29 16:21:01 -0600

    ADDED setPinMode

--- hardware/test.ino
+++ hardware/test.ino
@@ -1,5 +1,35 @@
 int led = D0;
 int ledState = 0;
+int setPinMode(String args)
+{
+  unsigned int loc = 0;
+  unsigned int  pin = 0;
+  unsigned int  mode = 0;
+
+
+  loc = command.indexOf(",");
+  pin = command.substring(0,loc).toInt();
+
+  mode = command.substring(loc+1).toInt();
+  // Error check
+  if(mode != 0 && mode != 1)
+  {
+    //invalid mode
+    return 0;
+  }
+  if(pin > 16)
+  {
+    return 0;
+  }
+  if(mode == 0)
+  {
+    pinMode(pin,OUTPUT);
+  }
+  else
+  {
+    pinMode(pin,INPUT);
+  }
+}
 int toggleLight(String args)
   {
     if(ledState == 1)
@@ -17,6 +47,8 @@ void setup() {
   pinMode(led, OUTPUT);
   Spark.variable("lights", &ledState, INT);
   Spark.function("toggleLight", toggleLight);
+    Spark.function("setPinMode", setPinMode);
+
 }
 
 void loop() {
